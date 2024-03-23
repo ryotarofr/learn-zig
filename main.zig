ee305d79be5baa239ba5037ba03878c3f6bfa792
RyotaroFujii <fujii.ryotaro@jenesty.co.jp>
2024-03-23 18:25:19 +0900

    スライス

--- main.zig
+++ main.zig
@@ -1,1 +1,25 @@
-pub fn main() void {}
+/// スライス
+const std = @import("std");
+pub fn main() void {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    const slice2 = array[0..];
+    std.debug.print("total(slice) = {}\n", .{total(slice)});
+    std.debug.print("total(slice2) = {}\n", .{total(slice2)});
+    try sliceTest();
+}
+
+fn total(values: []const u8) usize {
+    var sum: usize = 0;
+    for (values) |v| sum += v;
+    return sum;
+}
+
+pub fn sliceTest() !void {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    if (@TypeOf(slice) != *const [3]u8) {
+        return error.TypeMismatch;
+    }
+    std.debug.print("Type matches\n", .{});
+}
--- test_pass.zig
+++ test_pass.zig
@@ -1,15 +1,7 @@
-// run : zig test test_pass.zig
-
-/// usizeとisizeは、Zig言語の組み込み型で、それぞれ符号なし整数（unsigned integer）と符号付き整数（signed integer）を表します。
-/// usizeは符号なし整数を表し、その範囲は0から最大値までです。
-/// 最大値はプラットフォームによりますが、32ビットシステムでは2^32 - 1、64ビットシステムでは2^64 - 1となります。
-/// isizeは符号付き整数を表し、その範囲は最小値から最大値までです。
-/// 最小値と最大値はプラットフォームによりますが、32ビットシステムでは-2^31から2^31 - 1、64ビットシステムでは-2^63から2^63 - 1となります。
-/// これらの型は、特に配列のインデックスやオブジェクトのサイズを表すときによく使用されます。
-/// また、usizeとisizeのサイズは、実行しているシステムのアーキテクチャ（32ビットまたは64ビット）に依存します。
+/// run : zig test test_pass.zig
 const expect = @import("std").testing.expect;
-
-test "usize" {
-    try expect(@sizeOf(usize) == @sizeOf(*u8));
-    try expect(@sizeOf(isize) == @sizeOf(*u8));
+test "slices 2" {
+    const array = [_]u8{ 1, 2, 3, 4, 5 };
+    const slice = array[0..3];
+    try expect(@TypeOf(slice) == *const [3]u8);
 }
